3d279627afa0d62c38b217e7cd219dc612171c59
skoogiz <skoogiz@gmail.com>
2024-08-20 09:35:52 +0200

    lint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,12 +5,24 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    // ...tseslint.configs.recommendedTypeChecked,
+    // ...tseslint.configs.strict,
+    // ...tseslint.configs.stylistic,
+    // ...tseslint.configs.strictTypeChecked,
+    // ...tseslint.configs.stylisticTypeChecked,
+  ],
   files: ["**/*.{js,jsx,ts,tsx}"],
   ignores: ["dist"],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    // parserOptions: {
+    //   project: "./tsconfig.json",
+    //   tsconfigRootDir: import.meta.dirname,
+    // },
   },
   plugins: {
     "react-hooks": reactHooks,
